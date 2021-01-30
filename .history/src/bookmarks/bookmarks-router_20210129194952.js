35ac2df5160d62c27c0b4376b9c6262bf726328d
Kaitlyn Midori <katyluhoo@yahoo.com>
2021-01-29 19:58:27 -0800

    API refractor

--- .history/src/bookmarks/bookmarks-router_20210129194952.js
+++ .history/src/bookmarks/bookmarks-router_20210129194952.js
@@ -110,7 +110,7 @@ const express = require('express')
 const xss = require('xss')
 const logger = require('../logger')
 const BookmarksService = require('./bookmarks-service')
-const { getBookmarkValidationError } = require('./bookmark-validator')
+// const { getBookmarkValidationError } = require('./bookmark-validator')
 
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
--- migrations/002.do.alter_bookmarks_url.sql
+++
@@ -1,7 +0,0 @@
--- CREATE TYPE bookmarks_category AS ENUM (
---     'url'
--- );
-
--- ALTER TABLE bookmarks
---     ADD COLUMN
---         style
